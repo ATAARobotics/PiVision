62d8af9667ba117fb9b511a68b72e9bc8eb8111a
Matthew Naruzny <matthew_naruzny@shaw.ca>
2019-02-18 14:16:19 -0700

    Removed unused imports

--- piCode/src/main/java/ca/fourthreethreefour/EasyTables.java
+++ piCode/src/main/java/ca/fourthreethreefour/EasyTables.java
@@ -1,13 +1,13 @@
 package ca.fourthreethreefour;
 
-import edu.wpi.first.networktables.NetworkTableInstance;
+
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.networktables.NetworkTable;
+
 import edu.wpi.first.networktables.NetworkTableEntry;
 
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
 
 
 public class EasyTables {
@@ -24,7 +24,7 @@ public class EasyTables {
 
     public void updateDirection(Double drive){
         DRIVE_ENTRY_SHUFFLE.setDouble(drive);
-
+ 
     }
 
     public void updateSpeed(Double speed){
