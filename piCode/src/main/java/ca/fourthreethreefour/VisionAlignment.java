162347d0240a507a40aab0d819b46fa543746751
THATW33B <Nappa.pakka@gmail.com>
2019-02-19 23:56:06 -0700

    Whitespace

--- piCode/src/main/java/ca/fourthreethreefour/VisionAlignment.java
+++ piCode/src/main/java/ca/fourthreethreefour/VisionAlignment.java
@@ -19,7 +19,7 @@ public class VisionAlignment{
     private static final int IMG_WIDTH = 320;
     private static final int IMG_HEIGHT = 240;
     private static final int FOV = 60;
-
+    
     private final Object imgLock = new Object();
     
     CvSink cvSink = CameraServer.getInstance().getVideo();
@@ -27,105 +27,101 @@ public class VisionAlignment{
     //TODO Remove once feedback is not required
     CvSource outputStream = CameraServer.getInstance().putVideo("Image Analysis", IMG_WIDTH, IMG_HEIGHT);
     
-    // public VisionAlignment(){
-        // }
+    public RotatedRect[] findTargets(MyPipeline pipeline, Mat source){
+        pipeline.process(source);
+        RotatedRect[] visionTarget = {new RotatedRect(), new RotatedRect()};
         
-        
-        public RotatedRect[] findTargets(MyPipeline pipeline, Mat source){
-            pipeline.process(source);
-            RotatedRect[] visionTarget = {new RotatedRect(), new RotatedRect()};
-            
-            if (pipeline.filterContoursOutput().size() < 2) {
-                return visionTarget;
-            }
-            
-            //TODO test best way of determining target validity
-            // Determines the two largest rectangles puts them in visionTarget
-            for (int i = 0; i < pipeline.filterContoursOutput().size(); i++) {
-                
-                MatOfPoint mop = pipeline.filterContoursOutput().get(i);
-                MatOfPoint2f mop2f = new MatOfPoint2f();
-                
-                mop2f.fromArray(mop.toArray());
-                // Creates temporary object
-                RotatedRect currentRectangle = Imgproc.minAreaRect(mop2f);
-                
-                // If the current rectangle is larger than our largest
-                if (currentRectangle.size.area() > visionTarget[0].size.area()) {
-                    
-                    // Changes largest target to second largest
-                    visionTarget[1] = visionTarget[0];
-                    
-                    // Changes largest target to current target
-                    visionTarget[0] = currentRectangle;
-                }
-                
-                // If the current rectangle is larger thanm the second largest
-                else if (currentRectangle.size.area() > visionTarget[1].size.area()) {
-                    
-                    // Changes second largest target to current rectangles
-                    visionTarget[1] = currentRectangle;
-                }
-            }
-            
-            // Synchronizes data to prevent acess to variables
-            synchronized (imgLock) {
-                
-            }
+        if (pipeline.filterContoursOutput().size() < 2) {
             return visionTarget;
         }
         
-        public void updateVideo(RotatedRect[] visionTargets, Mat source){
-            //Draws Rectangle
-            Point[] boxPoints = new Point[4];
-            visionTargets[0].points(boxPoints);
-            for (int j = 0; j < 4; j++) {
-                Imgproc.line(source, boxPoints[j], boxPoints[(j + 1) % 4], new Scalar(0,0,255), 3);
+        //TODO test best way of determining target validity
+        // Determines the two largest rectangles puts them in visionTarget
+        for (int i = 0; i < pipeline.filterContoursOutput().size(); i++) {
+            
+            MatOfPoint mop = pipeline.filterContoursOutput().get(i);
+            MatOfPoint2f mop2f = new MatOfPoint2f();
+            
+            mop2f.fromArray(mop.toArray());
+            // Creates temporary object
+            RotatedRect currentRectangle = Imgproc.minAreaRect(mop2f);
+            
+            // If the current rectangle is larger than our largest
+            if (currentRectangle.size.area() > visionTarget[0].size.area()) {
+                
+                // Changes largest target to second largest
+                visionTarget[1] = visionTarget[0];
+                
+                // Changes largest target to current target
+                visionTarget[0] = currentRectangle;
             }
-            visionTargets[1].points(boxPoints);
-            for (int j = 0; j < 4; j++) {
-                Imgproc.line(source, boxPoints[j], boxPoints[(j + 1) % 4], new Scalar(0,0,255), 3);
+            
+            // If the current rectangle is larger thanm the second largest
+            else if (currentRectangle.size.area() > visionTarget[1].size.area()) {
+                
+                // Changes second largest target to current rectangles
+                visionTarget[1] = currentRectangle;
             }
-
-            //Send Frame
-            outputStream.putFrame(source);
+        }
+        
+        // Synchronizes data to prevent acess to variables
+        synchronized (imgLock) {
+            
+        }
+        return visionTarget;
+    }
+    
+    public void updateVideo(RotatedRect[] visionTargets, Mat source){
+        //Draws Rectangle
+        Point[] boxPoints = new Point[4];
+        visionTargets[0].points(boxPoints);
+        for (int j = 0; j < 4; j++) {
+            Imgproc.line(source, boxPoints[j], boxPoints[(j + 1) % 4], new Scalar(0,0,255), 3);
+        }
+        visionTargets[1].points(boxPoints);
+        for (int j = 0; j < 4; j++) {
+            Imgproc.line(source, boxPoints[j], boxPoints[(j + 1) % 4], new Scalar(0,0,255), 3);
+        }
+        
+        //Send Frame
+        outputStream.putFrame(source);
+        
+    }
+    
+    public double alignValues(RotatedRect[] visionTargets){
+        double centerX;
+        double centerX2;
+        double finalCenterX;
+        double turn = 0;
+        double focalLength;
+        double angleToTarget;
+        synchronized (imgLock) {
+            //Calculates CenterX 
+            centerX = visionTargets[0].center.x;
+            centerX2 = visionTargets[1].center.x;
+            finalCenterX = (centerX + centerX2) / 2;
+            //Calculates focalLength of camera
+            focalLength = IMG_WIDTH/(2*Math.tan(FOV/2));
             
         }
         
-        public double alignValues(RotatedRect[] visionTargets){
-            double centerX;
-            double centerX2;
-            double finalCenterX;
-            double turn = 0;
-            double focalLength;
-            double angleToTarget;
-            synchronized (imgLock) {
-                //Calculates CenterX 
-                centerX = visionTargets[0].center.x;
-                centerX2 = visionTargets[1].center.x;
-                finalCenterX = (centerX + centerX2) / 2;
-                //Calculates focalLength of camera
-                focalLength = IMG_WIDTH/(2*Math.tan(FOV/2));
-                
-            }
+        if(visionTargets.length == 2){
+            //TODO add proper turning calcs 
             
-            if(visionTargets.length == 2){
-                //TODO add proper turning calcs 
-                
-                //Calculates angle to the target
-                angleToTarget = Math.atan((finalCenterX - 159.5) / focalLength);
-                System.out.print(angleToTarget);
-                
-                
-                /*TODO: Use encoder values to turn with the angle
-                We can use encoder.getSelectedSensorPosition? and divide by 3 000 000? to get cm
-                Then we can tell the encoder to move x cm depending on the angle
-                (This will require a lot of testing and trial/error)
-                The ideal is making angleToTarget equal to 0 
-                (Note: Above calculations are not tested and I may have misinterpreted how to do them)
-                */
-            }
+            //Calculates angle to the target
+            angleToTarget = Math.atan((finalCenterX - 159.5) / focalLength);
+            System.out.print(angleToTarget);
             
-            return(turn);
+            
+            /*TODO: Use encoder values to turn with the angle
+            We can use encoder.getSelectedSensorPosition? and divide by 3 000 000? to get cm
+            Then we can tell the encoder to move x cm depending on the angle
+            (This will require a lot of testing and trial/error)
+            The ideal is making angleToTarget equal to 0 
+            (Note: Above calculations are not tested and I may have misinterpreted how to do them)
+            */
         }
+        
+        return(turn);
     }
+}
