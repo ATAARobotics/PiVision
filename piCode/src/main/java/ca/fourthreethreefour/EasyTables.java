f76c978e32fedef3cbb60bdeff13695df560283a
Matthew Naruzny <matthew_naruzny@shaw.ca>
2019-02-26 21:30:18 -0700

    Add NetworkTable value for Target Status

--- piCode/src/main/java/ca/fourthreethreefour/EasyTables.java
+++ piCode/src/main/java/ca/fourthreethreefour/EasyTables.java
@@ -17,12 +17,14 @@ public class EasyTables {
         this.inst = inst;
         NetworkTable table = inst.getTable("datatable");
         VISION_DRIVE_VALUE = table.getEntry("VISION_DRIVE_VALUE");
+        VISION_ERROR_NOTARGET = table.getEntry("VISION_ERROR_NOTARGET");
     }
 
     ShuffleboardTab dynamicSettingsTab = Shuffleboard.getTab("Dynamic Settings");
   
     NetworkTableEntry VISION_ACTIVE_ENTRY_SHUFFLE = dynamicSettingsTab.addPersistent("Vision Active", false).getEntry();
     NetworkTableEntry VISION_DRIVE_VALUE;
+    NetworkTableEntry VISION_ERROR_NOTARGET;
 
     public void updateDirection(Double drive){
         VISION_DRIVE_VALUE.setDouble(drive);
@@ -32,6 +34,10 @@ public class EasyTables {
 
     public boolean isVisionActive(){
         return(VISION_ACTIVE_ENTRY_SHUFFLE.getBoolean(false));
+    }
+
+    public void setNoTargetError(Boolean errorTrue){
+        VISION_ERROR_NOTARGET.setBoolean(errorTrue);
     }
     
 }
